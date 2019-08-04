e43e8f4f775d090f22e0758c0e9358c94eb01e16
Harm <harmschnakenberg@gmx.de>
2019-08-04 19:35:31 +0200

    Projektdateien hinzufügen.

---
+++ DDE2TCP/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DDE2TCP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
